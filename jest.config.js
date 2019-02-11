88296e786d19cef8debd5f7a5674c918ef9e87aa
Fabian Schneider <fabbbbbi+git@googlemail.com>
2019-02-11 19:30:23 +0100

    :white_check_mark: Add test framework

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts'
+  ]
+}
---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+describe('getDiff', () => {
+  it('test', () => {
+    expect(true).toBe(true);
+  });
+});
