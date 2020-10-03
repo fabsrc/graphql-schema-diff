4102db7ca6b36b387f98b802367848637a858434
Fabian Schneider <fabbbbbi+git@googlemail.com>
2020-10-03 18:30:41 +0200

    :arrow_up: Update packages

--- @types/disparity/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'disparity' {
-  interface Options {
-    paths?: [string?, string?]
-    context?: number
-  }
-
-  export function unified(str1: string, str2: string, opts?: Options): string;
-  export function unifiedNoColor(str1: string, str2: string, opts?: Options): string;
-}
