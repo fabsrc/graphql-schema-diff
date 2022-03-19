ee3d941b90ce4958464014a0a4277931abed8297
Fabian Schneider <fabbbbbi+git@googlemail.com>
2022-03-19 19:57:34 +0100

    :sparkles: Add syntax highlighting to html diff

--- src/html.ts
+++ src/html.ts
@@ -1,21 +1,37 @@
 import path from "path";
 import fs from "fs-extra";
-import { html } from "diff2html";
 
-const htmlTemplate = (diffHtml: string): string => `<html>
+const htmlTemplate = (diff: string): string => `<html>
 <head>
   <title>GraphQL Schema Diff</title>
-  <meta charset="utf-8" /> 
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="css/hljs.min.css">
   <link rel="stylesheet" href="css/diff2html.min.css">
+  <script src="js/highlight.min.js"></script>
+  <script src="js/graphql.min.js"></script>
   <script src="js/diff2html.min.js"></script>
-  <script src="js/diff2html-ui.min.js"></script>
-  <style>
-    html { box-sizing: border-box; }
-    *,*:before,*:after { box-sizing: inherit; }
-  </style>
+  <script src="js/diff2html-ui-base.min.js "></script>
 </head>
 <body>
-  ${diffHtml}
+  <div id="diff"></div>
+  <script>
+    document.addEventListener('DOMContentLoaded', () => {
+      const diffString = \`${diff}\`;
+      const targetElement = document.getElementById('diff');
+      const configuration = {
+        matching: 'lines',
+        outputFormat: 'side-by-side',
+        highlight: true,
+        synchronisedScroll: false,
+        drawFileList: false,
+        rawTemplates: {
+          'tag-file-renamed': '',
+        }
+      };
+      const diff2htmlUi = new Diff2HtmlUI(targetElement, diffString, configuration, hljs);
+      diff2htmlUi.draw();
+    });
+  </script>
 </body>
 </html>`;
 
@@ -31,17 +47,54 @@ export async function createHtmlOutput(
 
   const adjustedDiff = diff
     .replace(/(---\s.*)\sremoved/, "$1")
-    .replace(/(\+\+\+\s.*)\sadded/, "$1");
-  const diffHtml = html(adjustedDiff, {
-    matching: "lines",
-    outputFormat: "side-by-side",
-    rawTemplates: {
-      "tag-file-renamed": "",
-    },
-  });
+    .replace(/(\+\+\+\s.*)\sadded/, "$1")
+    .replace("No newline at end of file", "");
+
   await fs.ensureDir(outputDirectory);
+
   const diff2HtmlPath = path.dirname(require.resolve("diff2html/package.json"));
-  await fs.copy(path.join(diff2HtmlPath, "bundles"), outputDirectory);
-  const htmlOutput = htmlTemplate(diffHtml);
-  await fs.writeFile(path.join(outputDirectory, "index.html"), htmlOutput);
+  const highlightJsPath = path.dirname(
+    require.resolve("@highlightjs/cdn-assets/package.json")
+  );
+
+  await Promise.all([
+    fs.copy(
+      path.join(diff2HtmlPath, "bundles/js/diff2html.min.js"),
+      path.join(outputDirectory, "js/diff2html.min.js")
+    ),
+    fs.copy(
+      path.join(diff2HtmlPath, "bundles/css/diff2html.min.css"),
+      path.join(outputDirectory, "css/diff2html.min.css")
+    ),
+    fs.copy(
+      path.join(highlightJsPath, "styles/default.min.css"),
+      path.join(outputDirectory, "css/hljs.min.css")
+    ),
+    fs.copy(
+      path.join(highlightJsPath, "highlight.min.js"),
+      path.join(outputDirectory, "js/highlight.min.js")
+    ),
+    fs.copy(
+      path.join(highlightJsPath, "languages/graphql.min.js"),
+      path.join(outputDirectory, "js/graphql.min.js")
+    ),
+  ]);
+
+  const diff2htmlUiBase = (
+    await fs.readFile(
+      path.join(diff2HtmlPath, "bundles/js/diff2html-ui-base.min.js")
+    )
+  )
+    .toString()
+    // Hacky way to force GraphQL syntax highlighting
+    .replace('void 0!==t?t:"plaintext"', '"graphql"');
+
+  const htmlOutput = htmlTemplate(adjustedDiff);
+  await Promise.all([
+    fs.writeFile(path.join(outputDirectory, "index.html"), htmlOutput),
+    fs.writeFile(
+      path.join(outputDirectory, "js/diff2html-ui-base.min.js"),
+      diff2htmlUiBase
+    ),
+  ]);
 }
